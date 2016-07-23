4a068d87868f1c74fd6fc9115ed51c8e41eb9296
alteleid24 <alteleid24@gmail.com>
2016-07-23 22:57:23 +0300

    Update Revision3.js

--- Revision3.js
+++ Revision3.js
@@ -85,21 +85,13 @@ handlers.makeSparePartActive = function (args, context) {
 	log.info("parsedActiveParts = "+parsedActiveParts);
 	
 	var partItem = characterInventory.find((pi) => { return pi.ItemInstanceId == args.ItemInstanceId; });
-	/*
+
 	var carParamsKey = server.ExecuteCloudScript(
 	{
 		PlayFabId: currentPlayerId,
 		FunctionName: "convertToCarParamsKey",
 		FunctionParameter: { itemClass: partItem.ItemClass }
 	}).FunctionResult["resultKey"];
-	*/
-	var carParamsKey = server.ExecuteCloudScript(
-	{
-		PlayFabId: currentPlayerId,
-		FunctionName: "convertToCarParamsKey",
-		FunctionParameter: { itemClass: partItem.ItemClass },
-		GeneratePlayStreamEvent: true
-	});
 	
 	log.info("carParamsKey = "+carParamsKey);
 	
